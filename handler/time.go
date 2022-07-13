09ae3c4855a989ee2f26c14ee3ba2ed72b238172
Erfan Diakoo <diakoo123@gmail.com>
2022-07-13 22:33:51 +0430

    first commit

---
+++ handler/time.go
@@ -0,0 +1,66 @@
+package handler
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/erfandiakoo/TimeApi/models"
+	"github.com/gofiber/fiber/v2"
+)
+
+func GetTime(c *fiber.Ctx) error {
+	dt := time.Now()
+	return c.JSON(&models.ResponseModel{
+		Data:    map[string]interface{}{"result": dt},
+		Message: "Ok",
+		Code:    200,
+	})
+}
+
+func GetTimeUnix(c *fiber.Ctx) error {
+	dt := time.Now().UnixNano()
+	return c.JSON(&models.ResponseModel{
+		Data:    map[string]interface{}{"result": dt},
+		Message: "Ok",
+		Code:    200,
+	})
+}
+
+func GetTimeUtc(c *fiber.Ctx) error {
+	dt := time.Now().UTC()
+	return c.JSON(&models.ResponseModel{
+		Data:    map[string]interface{}{"result": dt},
+		Message: "Ok",
+		Code:    200,
+	})
+}
+
+func GetTimeIP(c *fiber.Ctx) error {
+	timezone := GetIPZone(string(c.IP()))
+	fmt.Println(timezone)
+	loc, _ := time.LoadLocation(timezone)
+	dt := time.Now().In(loc)
+	return c.JSON(&models.ResponseModel{
+		Data:    map[string]interface{}{"result": dt},
+		Message: "Ok",
+		Code:    200,
+	})
+}
+func GetIPZone(ip string) string {
+	resp, err := http.Get("http://ip-api.com/json/" + ip + "?fields=256")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(body))
+	return string(body)
+}
---
+++ router/router.go
@@ -0,0 +1,20 @@
+package router
+
+import (
+	"github.com/erfandiakoo/TimeApi/handler"
+	"github.com/gofiber/fiber/v2"
+)
+
+func SetupRoutes(app *fiber.App) {
+	api := app.Group("/")
+	api.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON("Worked")
+	})
+
+	time := api.Group("api/v1")
+	time.Get("/now", handler.GetTime)
+	time.Get("/unix", handler.GetTimeUnix)
+	time.Get("/utc", handler.GetTimeUtc)
+	time.Get("/ip", handler.GetTimeIP)
+
+}
